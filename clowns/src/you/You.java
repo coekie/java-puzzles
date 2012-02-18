f43053ff711715b64340cbb12dfbec66339ad3e0
Wouter Coekaerts <wouter@coekaerts.be>
2012-02-18 23:54:36 +0100

    Add solution: clowns

--- clowns/src/you/You.java
+++ clowns/src/you/You.java
@@ -4,12 +4,20 @@ import clowns.Clown;
 import clowns.Volkswagen;
 
 public class You {
+    static int counter = 0;
+    static Volkswagen vw = new Volkswagen();
+
     public static void main(String args[]) {
-        // TODO put 20 clowns into a Volkswagen
-        Volkswagen vw = new Volkswagen();
-        for (int i = 0; i < 20; i++) {
-            vw.add(new Clown());
-        }
+        vw.add(new RecursiveClown());
         vw.done();
+    }
+
+    static class RecursiveClown extends Clown {
+        public int hashCode() {
+            if (++counter < 20) {
+                vw.add(new RecursiveClown());
+            }
+            return super.hashCode();
+        }
     }
 }
