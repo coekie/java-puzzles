3543e5eccd29aa491ed3d05f08a0d1858513fec8
Wouter Coekaerts <wouter@coekaerts.be>
2012-03-04 17:38:43 +0100

    Add solution: car part 2

--- car2/src/driver/Driver.java
+++ car2/src/driver/Driver.java
@@ -3,10 +3,27 @@ package driver;
 import car.Car;
 
 public class Driver {
+    static Car car = new Car();
+    static int a = 0;
+
     public static void main(String args[]) {
-        // TODO break the speed limit
-        Car car = new Car();
-        car.accelerate(1001);
+        recurse();
         car.vroom();
+    }
+
+    static void recurse() {
+        try {
+            recurse(); // recurse without the car
+        } catch (StackOverflowError e) {
+            // when we've hit the limit of the stack, just go back out
+        }
+        if (a++ == 10) { // after taking 10 steps back
+            recurse2(); // recurse with the car
+        }
+    }
+
+    static void recurse2() {
+        car.accelerate(1001);
+        recurse2();
     }
 }
