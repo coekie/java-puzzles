bbb16630571a44f0dc191c5cfa9d56ab8584f943
Wouter Coekaerts <wouter@coekaerts.be>
2012-03-26 19:56:24 +0200

    Add solution: chicken

--- chicken/src/creator/Creator.java
+++ chicken/src/creator/Creator.java
@@ -1,9 +1,30 @@
 package creator;
 
 import chicken.Chicken;
+import chicken.Egg;
 
 public class Creator {
-    public static void main(String[] args) {
-        new Chicken(null).ask();
+    static class FirstEgg extends Egg {
+        FirstEgg() {
+            super(null);
+        }
+
+        @Override
+        protected void finalize() {
+            new Chicken(this).ask();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            new FirstEgg();
+        } catch (NullPointerException e) {
+        }
+
+        // there are ways to force garbage collection harder,
+        // but this works good enough for me
+        System.gc();
+        System.runFinalization();
+        Thread.sleep(1000);
     }
 }
