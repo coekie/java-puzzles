bad81b35eee467478fe7257845dc1e5a792e15c0
Wouter Coekaerts <wouter@coekaerts.be>
2012-08-12 23:39:28 +0200

    Add solution: cookies

--- cookies/src/monster/CookieMonster.java
+++ cookies/src/monster/CookieMonster.java
@@ -1,7 +1,19 @@
 package monster;
 
 public class CookieMonster {
-    public static String eat(String cookie) {
-        return cookie.substring(0, cookie.length() - 6);
+    public static <C extends CharSequence> C eat(String cookie) {
+        return (C) (cookie.length() > 0 ? "" : new CharSequence() {
+            public int length() {
+                return -1;
+            }
+
+            public char charAt(int index) {
+                throw new UnsupportedOperationException();
+            }
+
+            public CharSequence subSequence(int start, int end) {
+                throw new UnsupportedOperationException();
+            }
+        });
     }
 }
